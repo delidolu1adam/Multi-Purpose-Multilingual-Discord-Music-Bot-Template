3e92f6c7623624411745f45f9cfe242a486fe046
Serhat Öktem <guazys@gmail.com>
2024-03-09 13:41:47 +0100

    Update config.js

--- requires/config.js
+++ requires/config.js
@@ -1,1 +1,23 @@
+module.exports = {
 
+  general: {
+  BOT_TOKEN: 'BOT_TOKEN', // Bot token
+  BOT_STATUS: 'true', // Custom status message enabled or disabled
+  BOT_STATUS_MESSAGE: 'It is developed ❤️ Guazy', // Custom status message
+  GUILD_ID: 'TYPE-HERE', // Your discord server ID
+  DEVELOPER_ID: 'TYPE-HERE', // Your discord client ID
+  INVITE_URL: 'TYPE-HERE' // Bot invite URL
+  },
+
+  options: {
+  PREMIUM_MODE: 'false', // Premium mode enabled or disabled
+  DEFAULT_LANGUAGE: 'en', // Default language
+  DEFAULT_VOLUME: 50, // Default volume level
+  },
+
+  api: {
+  SPOTIFY_CLIENT_ID: 'TYPE-HERE', // Spotify client ID
+  SPOTIFY_SECRET_KEY: 'TYPE-HERE', // Spotify secret KEY
+  },
+
+};
