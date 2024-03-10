c58e616b94f9c5db4e258f04a5f4c862cdcc7a9d
Serhat Öktem <guazys@gmail.com>
2024-03-10 15:38:23 +0100

    Update radio.js

--- commands/radio/radio.js
+++ commands/radio/radio.js
@@ -1,1 +1,16 @@
-// Radio player with URL and Stations List
+// Live radio URL or radio list from stations.txt file.
+
+For example command use
+
+/radio URL
+
+or
+
+/radio stations
+
+1: Radio Name
+2: Radio Name
+3: Radio Name
+4: Radio Name
+5: Radio Name
+< Previous Button - Next Button >
